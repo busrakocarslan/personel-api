37308a0a036104162f4df96ffbf75b5d53ad94e1
busra <kocarslanbusrab@gmail.com>
2024-08-16 12:03:45 +0300

    morgan change

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
@@ -6,9 +6,8 @@
 //LOGGER
 //npm i morgan
 //https://expressjs.com/en/resources/middleware/morgan.html
-const morgan = require('morgan')
-app.use(morgan('tiny'))
-
+// const morgan = require('morgan')
+// app.use(morgan('tiny'))
 
 // app.use(morgan('short'))
 //app.use(morgan ('dev'))
@@ -16,34 +15,33 @@ app.use(morgan('tiny'))
 // app.use(morgan('combined'))
 
 //Custom Log:
-app.use(morgan('TIME=":date[iso]" - URL=":url" - Method=":method" - IP=":remote-addr" - Ref=":referrer" - Status=":status" - Sign=":user-agent" (:response-time[digits] ms)'))
-
-
+app.use(
+  morgan(
+    'TIME=":date[iso]" - URL=":url" - Method=":method" - IP=":remote-addr" - Ref=":referrer" - Status=":status" - Sign=":user-agent" (:response-time[digits] ms)'
+  )
+);
 
 //? custom olarak kendimiz düzenlemek istersek örnek bu dökümandan bak!
 
 // Write to File: A
 
-
 // npm i morgan--> log kayıtlearının tutmak için olan ilgili modul
 //LOGGER
-// morgen defaul olarak consola  yazar. Dosyaya yazması gerek. 
-const morgan=require('morgan')
-const fs=require('node:fs')//  file system modulü, dahili modul tuttuğumuz log kayıtlarını dosyalayacak
+// morgen defaul olarak consola  yazar. Dosyaya yazması gerek.
+const morgan = require("morgan");
+const fs = require("node:fs"); //  file system modulü, dahili modul tuttuğumuz log kayıtlarını dosyalayacak
+const now = new Date();
+const today = now.toISOString().split("T")[0];
 // const now=new Date()
-// const today=now.toISOString().split('T')[0]//? now ile gelen tarih bir obje bunun toIsoString ile stringe çevirmemiz gerekiyor. Stringe çevirdikten sonra da split methodu ile istediğimiz yerinden ayırabiliyoruz tarihi. Split te bize array döndüğünden ilk elemanı bizim istediğimiz tarih olduğundan 0 indisi ile tam istediğimiz tarihe ulaşıyoruz. 
+// const today=now.toISOString().split('T')[0]//? now ile gelen tarih bir obje bunun toIsoString ile stringe çevirmemiz gerekiyor. Stringe çevirdikten sonra da split methodu ile istediğimiz yerinden ayırabiliyoruz tarihi. Split te bize array döndüğünden ilk elemanı bizim istediğimiz tarih olduğundan 0 indisi ile tam istediğimiz tarihe ulaşıyoruz.
 
 // app.use(morgan('combined',{
 //     stream:fs.createWriteStream(`.logs/${today}.log`,{flags:'+a'})
-// }))// middleware olduğundan app.use içinde çağırılıyor ve hazır formatlardan biri (içine) yazılıyor. 
+// }))// middleware olduğundan app.use içinde çağırılıyor ve hazır formatlardan biri (içine) yazılıyor.
 
-const now=new Date()
-const today=now.toISOString().split('T')[0]
-module.exports=morgan('combined',{
-        stream:fs.createWriteStream(`./logs/${today}.log`,{flags:'a+'})})
+module.exports = morgan("combined", {
+  stream: fs.createWriteStream(`./logs/${today}.log`, { flags: "a+" }),
+});
 
-
-
-        
-//stream akış demek. akışta fs modulünü aç 
-//createWriteStream= akışm olduğunda yaz 
+//stream akış demek. akışta fs modulünü aç
+//createWriteStream= akış olduğunda yaz
